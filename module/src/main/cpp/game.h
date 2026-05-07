367cee2f817536ffbcfc583d55668bf3b307d740
GravityRushTCO <frederic59261@gmail.com>
2026-05-07 18:09:32 +0200

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.twoheadedshark.tco"
+#define GamePackageName "com.Chillgaming.oneState"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
